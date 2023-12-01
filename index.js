34c9f2a14a30e5b6f19b47e437b42f83df24b184
farrux <orziyevfarrux01@gmail.com>
2023-12-01 23:33:12 +0500

    added message

--- index.js
+++ index.js
@@ -119,6 +119,295 @@ bot.on("location", async (msg) => {
   );
 });
 
+bot.on("message", async (msg) => {
+  if (msg.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg.web_app_data.data);
+      if (msg.web_app_data.data.length >= 0) {
+        let user = await client.query(
+          "SELECT * FROM users where user_id = $1",
+          [msg.from.id]
+        );
+
+        if (data.payment !== "Click" && data.payment !== "Payme") {
+          let create = await client.query(
+            "INSERT INTO orders(products, total, user_id, username, phone_number, comment, payment_type, exportation, payment_status) values($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+            [
+              data.order_products,
+              `${data?.total}`,
+              msg.from.id,
+              msg.from.first_name,
+              user.rows[0].phone_number,
+              data.comment,
+              data.payment,
+              data.delivery,
+              true,
+            ]
+          );
+        } else {
+          let create = await client.query(
+            "INSERT INTO orders(products, total, user_id, username, phone_number, comment, payment_type, exportation, payment_status) values($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+            [
+              data.order_products,
+              `${data?.total}`,
+              msg.from.id,
+              msg.from.first_name,
+              user.rows[0].phone_number,
+              data.comment,
+              data.payment,
+              data.delivery,
+              false,
+            ]
+          );
+        }
+
+        let percentagePromo = "";
+        if (data.promocode !== "") {
+          let getPromo = await client.query(
+            "SELECT * FROM promocode WHERE title = $1 AND isActive = true",
+            [data.promocode]
+          );
+          const order = await client.query(
+            "SELECT * FROM orders WHERE user_id = $1",
+            [msg.from.id]
+          );
+
+          percentagePromo = `${getPromo.rows[0].sale}`;
+          let orders_id = [];
+          let usedCount = getPromo.rows[0]?.usedcount + 1;
+          if (
+            getPromo.rows[0].orders_id !== undefined &&
+            getPromo.rows[0].orders_id?.length > 0
+          ) {
+            for (let i = 0; i < getPromo.rows[0].orders_id.length; i++) {
+              orders_id.push(getPromo.rows[0].orders_id[i]);
+            }
+            orders_id.push(order.rows[order.rows.length - 1].order_id);
+          } else {
+            orders_id.push(order.rows[order.rows.length - 1].order_id);
+          }
+
+          let updatePromo = await client.query(
+            "UPDATE promocode SET usedCount = $1, orders_id = $2 WHERE id = $3",
+            [usedCount, orders_id, getPromo.rows[0].id]
+          );
+
+          if (getPromo.rows[0].usage_limit == getPromo.rows[0].usedcount + 1) {
+            let updatePromo = await client.query(
+              "UPDATE promocode SET isActive = false WHERE id = $1",
+              [getPromo.rows[0].id]
+            );
+          }
+        }
+        let getCount = await client.query("SELECT MAX(count) FROM orders");
+
+        let startSum = 10000;
+        let kmSum = 2000;
+
+        // Convert from degrees to radians
+        function degreesToRadians(degrees) {
+          var radians = (degrees * Math.PI) / 180;
+          return radians;
+        }
+
+        // Function takes two objects, that contain coordinates to a starting and destination location.
+        function calcDistance(startingCoords, destinationCoords) {
+          let startingLat = degreesToRadians(startingCoords.latitude);
+          let startingLong = degreesToRadians(startingCoords.longitude);
+          let destinationLat = degreesToRadians(destinationCoords.latitude);
+          let destinationLong = degreesToRadians(destinationCoords.longitude);
+
+          // Radius of the Earth in kilometers
+          let radius = 6571;
+
+          // Haversine equation
+          let distanceInKilometers =
+            Math.acos(
+              Math.sin(startingLat) * Math.sin(destinationLat) +
+                Math.cos(startingLat) *
+                  Math.cos(destinationLat) *
+                  Math.cos(startingLong - destinationLong)
+            ) * radius;
+
+          return distanceInKilometers;
+        }
+
+        let sCoords = {
+          latitude: 41.302626,
+          longitude: 69.279813,
+        };
+
+        let dCoords = {
+          latitude: user.rows[0].user_location[0],
+          longitude: user.rows[0].user_location[1],
+        };
+
+        let dist = Math.round(calcDistance(sCoords, dCoords));
+        let resDeliveryPrice = dist * kmSum + startSum;
+
+        const order = await client.query(
+          "SELECT * FROM orders WHERE user_id = $1",
+          [msg.from.id]
+        );
+
+        let products = [];
+        for (
+          let p = 0;
+          p < order.rows[order.rows.length - 1].products.length;
+          p++
+        ) {
+          let productToJson = JSON.parse(
+            order.rows[order.rows.length - 1].products[p]
+          );
+          products.push(productToJson);
+        }
+
+        const token = process.env.TelegramApi;
+        const chat_id = process.env.CHAT_ID;
+        const message = `<b>Поступил заказ с Telegram бота:</b> ${
+          getCount.rows[0].max
+        } %0A
+  <b>Имя клиента:</b> ${msg.from.first_name} %0A
+  <b>Номер:</b> ${user.rows[0].phone_number} ${
+          msg.from.username !== undefined ? `| @${msg.from.username}` : ""
+        }%0A
+  <b>Адрес:</b> ${user.rows[0].reverse_location} (Локация после сообщения) %0A
+          %0A
+  <b>Дистанция:</b> ${dist}km%0A
+  <b>Оплате (${data.payment}) </b>%0A
+  <b>Тип выдачи:</b> ${data.delivery} %0A
+  <b>Комментарий: ${data.comment !== "" ? `${data.comment}` : "Нет"}</b> %0A
+  <b>Промокод: ${
+    data.promocode !== ""
+      ? `${data.promocode} - ${percentagePromo}%`
+      : "Не использован"
+  }</b> %0A
+  %0A
+  <b>Сумма заказа:</b> ${
+    data.delivery == "Доставка"
+      ? `${(data?.total - resDeliveryPrice).toLocaleString()}`
+      : `${(data?.total + 0).toLocaleString()}`
+  } UZS %0A
+  <b>Доставка:</b> ${
+    data.delivery == "Доставка"
+      ? ` ${resDeliveryPrice?.toLocaleString()} (${dist} km)`
+      : "Самовызов"
+  }%0A
+  <b>Итого:</b> ${(data?.total + 0).toLocaleString()} UZS%0A
+  %0A
+  <b>Товары в корзине:</b> ${products.map((i, index) => {
+    let text = ` %0A ${index + 1}. ${i.product_name} ${
+      i.filling !== "" ? `(${i.filling})` : ``
+    } %0A 
+    ${i.count} x ${i.price.replace(/\D/g, " ")} = ${
+      i.price.replace(/\D/g, "") * i.count
+    }`;
+    return text;
+  })} %0A
+        `;
+
+        if (data.payment == "Click") {
+          const order = await client.query(
+            "SELECT * FROM orders WHERE user_id = $1",
+            [msg.from.id]
+          );
+
+          var num = order.rows[order.rows.length - 1].total.replaceAll(
+            /\D/g,
+            ""
+          );
+          var resTotal = parseInt(num);
+
+          bot.sendMessage(msg.chat.id, `Оформления заказа`, {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: `Оплатить`,
+                    url: `https://my.click.uz/services/pay?service_id=${29813}&merchant_id=${22179}&amount=${+resTotal}&transaction_param=${
+                      order.rows[order.rows.length - 1].order_id
+                    }`,
+                  },
+                ],
+              ],
+              resize_keyboard: true,
+            },
+          });
+        } else if (data.payment == "Payme") {
+          const order = await client.query(
+            "SELECT * FROM orders WHERE user_id = $1",
+            [msg.from.id]
+          );
+
+          var num = order.rows[order.rows.length - 1].total.replaceAll(
+            /\D/g,
+            ""
+          );
+          var resTotal = parseInt(num);
+
+          // prepare payme request
+          const apiUrl = "https://checkout.paycom.uz/api";
+          const requestBody = {
+            jsonrpc: "2.0",
+            id: 1111111,
+            method: "receipts.create",
+            params: {
+              amount: resTotal * 100,
+              account: {
+                order_id: order.rows[order.rows.length - 1].order_id,
+              },
+            },
+          };
+          const headers = {
+            "X-Auth": `${process.env.PAYME_CASH_ID}:${process.env.PAYME_KEY}`,
+            "Content-Type": "application/json",
+          };
+          // send request to payme
+          const response = await axios.post(apiUrl, requestBody, { headers });
+          const transID = response.data.result.receipt._id;
+          bot.sendMessage(msg.chat.id, `Оформления заказа`, {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: `Оплатить`,
+                    url: `https://payme.uz/checkout/${transID}`,
+                  },
+                ],
+              ],
+              resize_keyboard: true,
+            },
+          });
+        } else {
+          await axios.post(
+            `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&parse_mode=html&text=${message}`
+          );
+
+          await axios.post(
+            `https://api.telegram.org/bot${token}/sendLocation?chat_id=${chat_id}&latitude=${user.rows[0].user_location[0]}&longitude=${user.rows[0].user_location[1]}`
+          );
+
+          await bot.sendMessage(
+            msg.chat.id,
+            `Ваш заказ принят! Cкоро оператор свяжется с вами! Спасибо за доверие 😊
+          Для нового заказа нажмите на кнопку "Отправить контакт"`,
+            {
+              reply_markup: JSON.stringify({
+                keyboard: [
+                  [{ text: "Отправить контакт", request_contact: true }],
+                ],
+                resize_keyboard: true,
+              }),
+            }
+          );
+        }
+      }
+    } catch (error) {
+      console.log("error ->", error);
+    }
+  }
+});
+
 app.use(loginRoute);
 app.use(categoryRoute);
 app.use(productRoute);
