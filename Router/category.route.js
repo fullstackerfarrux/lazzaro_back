33c041ab16c31bb32070287a9a91d8bbfed94d64
farrux <orziyevfarrux01@gmail.com>
2023-12-03 23:11:46 +0500

    added not product category

--- Router/category.route.js
+++ Router/category.route.js
@@ -3,12 +3,13 @@ import {
   createCategory,
   deleteCategory,
   getCategories,
+  getCategoriesNot,
 } from "../Controller/category.controller.js";
 const router = Router();
 
 router.post("/category/create", createCategory);
 router.post("/category/delete", deleteCategory);
 router.get("/categories", getCategories);
-router.get("/categories/not", getCategoriesNot)
+router.get("/categories/not", getCategoriesNot);
 
 export default router;
